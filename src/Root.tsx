31ca1225797b6c8e7901c745d6db18860edc778e
Muhammad Ahmed Hassan <51692318+MuhammadAhmedHassan@users.noreply.github.com>
2023-06-21 16:57:31 +0500

    update: added home and login routes and auth check

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,19 +1,68 @@
-import { gql, useMutation } from '@apollo/client';
-import { useEffect } from 'react';
+import { gql, useQuery } from '@apollo/client';
+import { PropsWithChildren, useEffect } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { MyTodos, Signup } from './pages';
+import { AppLocalStorage } from './utils/app-localstorage';
+import { AuthProvider, useAuth } from './utils/auth';
 
 export const Root = () => {
-  // to log in, get a json web token
-  // this account has been seeded to the database in advance
-  const [getToken, { data }] = useMutation(gql`
-    mutation Token {
-      token(email: "test@skand.io", password: "testtest")
+  const auth = useAuth();
+
+  const { loading, error } = useQuery(gql`
+    query GetUser {
+      getUser {
+        id
+        email
+      }
     }
   `);
 
   useEffect(() => {
-    getToken();
-  }, [getToken]);
+    if (error && error?.graphQLErrors.length > 0) {
+      AppLocalStorage.removeItem('user-token');
+    } else {
+      auth.setUser(AppLocalStorage.getItem('user-token'));
+    }
+  }, [error, auth]);
 
-  // simply show the token for now
-  return <>{data?.token}</>;
+  if (loading) return null;
+
+  return (
+    <AuthProvider>
+      <div className="h-screen w-screen bg-gray-300 pt-6">
+        <div className="h-full w-full sm:w-2/3 mx-auto">
+          <Routes>
+            <Route
+              path="/login"
+              element={
+                <NotProtected>
+                  <Signup />
+                </NotProtected>
+              }
+            />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <MyTodos />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </div>
+      </div>
+    </AuthProvider>
+  );
 };
+
+function ProtectedRoute({ children }: PropsWithChildren) {
+  const auth = useAuth();
+  if (!auth.user) return <Navigate to="/login" />;
+  return children;
+}
+
+function NotProtected({ children }: PropsWithChildren) {
+  const auth = useAuth();
+  if (auth.user) return <Navigate to="/" />;
+  return children;
+}
