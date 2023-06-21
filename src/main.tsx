9152a9c76e2d4629716f159447c2139f3f88a1b1
Muhammad Ahmed Hassan <51692318+MuhammadAhmedHassan@users.noreply.github.com>
2023-06-21 16:50:10 +0500

    update: send auth token with graphql request

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,41 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Root } from './Root';
 import './libraries/server'; // This does the magic
+import './index.css';
+import { AppLocalStorage } from './utils/app-localstorage';
+import { BrowserRouter } from 'react-router-dom';
+
+const httpLink = createHttpLink({
+  uri: '/graphql'
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = AppLocalStorage.getItem('user-token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  uri: '/graphql'
+  // uri: '/graphql',
+  link: authLink.concat(httpLink)
 });
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <Root />
+      <BrowserRouter>
+        <Root />
+      </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>
 );
